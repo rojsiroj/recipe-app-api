fb1596e3ed6e37112811bc6b05c745f199512d99
rojsiroj <sirojudin.dev@gmail.com>
2024-07-05 20:42:38 +0700

    chore: add test user admin and init user admin page

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,7 +13,8 @@ class ModelTests(TestCase):
         email = "test@example.com"
         password = "testpass123"
         user = get_user_model().objects.create_user(
-            email=email, password=password
+            email=email,
+            password=password,
         )
 
         self.assertEqual(user.email, email)
@@ -52,7 +53,8 @@ class ModelTests(TestCase):
     def test_create_superuser(self):
         # Test creating superuser
         user = get_user_model().objects.create_superuser(
-            "test@example.com", "test123"
+            "test@example.com",
+            "test123",
         )
 
         self.assertTrue(user.is_superuser)
