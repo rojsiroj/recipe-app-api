6ad334f33eb4d82bc2076017ae6663b0e0e52b40
rojsiroj <sirojudin.dev@gmail.com>
2024-07-05 14:09:48 +0700

    chore: hotfix example test error linter

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,15 +6,16 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     # Test the calc module
-    
+
     def test_add_numbers(self):
         res = calc.add(5, 6)
-        
+
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         res = calc.subtract(6, 5)
-        
+
         self.assertEqual(res, 1)
