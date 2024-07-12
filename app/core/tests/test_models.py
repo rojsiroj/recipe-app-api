7cdc55affad81c2fbbf2c93b8530dc7ca6a9ace3
rojsiroj <sirojudin.dev@gmail.com>
2024-07-12 09:03:09 +0700

    chore: write test for ingredient model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -94,3 +94,16 @@ class ModelTests(TestCase):
         tag = models.Tag.objects.create(user=user, name="Tag1")
 
         self.assertEqual(str(tag), tag.name)
+
+    """
+        Test Ingredient model starts here
+    """
+
+    def test_create_ingredient(self):
+        # Test creating an ingredient is successful
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user, name="Ingredient1"
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
