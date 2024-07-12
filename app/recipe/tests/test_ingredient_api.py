c918014a03483b687d5ae3b60da2eff7705f9a42
rojsiroj <sirojudin.dev@gmail.com>
2024-07-12 09:39:55 +0700

    chore: write test for delete ingredient API

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -76,3 +76,13 @@ class PrivateIngredientsAPITests(TestCase):
 
         ingredient.refresh_from_db()
         self.assertEqual(ingredient.name, payload["name"])
+
+    def test_delete_ingredient(self):
+        # Test deleting an ingredient
+        ingredient = Ingredient.objects.create(user=self.user, name="Salt")
+        url = detail_url(ingredient.id)
+        res = self.client.delete(url)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+
+        ingredients = Ingredient.objects.filter(user=self.user)
+        self.assertFalse(ingredients.exists())
