7dab40caf084b6864a2658c5c6609b3b69fbc701
rojsiroj <sirojudin.dev@gmail.com>
2024-07-13 14:43:47 +0700

    chore: add image field in recipe detail serializers

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -91,7 +91,7 @@ class RecipeSerializer(serializers.ModelSerializer):
 class RecipeDetailSerializer(RecipeSerializer):
     # Serializer for recipe detail view
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ["description"]
+        fields = RecipeSerializer.Meta.fields + ["description", "image"]
 
 
 class RecipeImageSerializer(serializers.ModelSerializer):
