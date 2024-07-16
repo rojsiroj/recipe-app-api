ab850712363f29be09912b2c61dfc32e361e9905
rojsiroj <sirojudin.dev@gmail.com>
2024-07-16 20:51:08 +0700

    chore: implement filtering tag and ingredient

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -98,6 +98,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
         )
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "assigned_only",
+                OpenApiTypes.INT,
+                enum=[0, 1],
+                description="Filter by item assigned to recipes",
+            )
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(
     mixins.ListModelMixin,
     mixins.UpdateModelMixin,
@@ -110,7 +122,17 @@ class BaseRecipeAttrViewSet(
 
     def get_queryset(self):
         # Retrieve tags for authenticated user
-        return self.queryset.filter(user=self.request.user).order_by("-name")
+        assigned_only = bool(
+            int(self.request.query_params.get("assigned_only", 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+        return (
+            queryset.filter(user=self.request.user)
+            .order_by("-name")
+            .distinct()
+        )
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
